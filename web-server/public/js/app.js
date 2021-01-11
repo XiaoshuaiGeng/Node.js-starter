a38a72448068680ac3db757509c76c7f90a4c0ed
Xiaoshuai Geng <xiaoshuaigeng@gmail.com>
2021-01-11 18:15:48 +0000

    update

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,21 @@
 console.log('client side js loaded')
 
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if (data.error){
-            console.log(data.error)
-        }else{
-            console.log(data)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+
+messageOne.textContent = ''
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    // console.log('Testing')
+    const location = search.value
+    fetch('http://localhost:3000/weather?address='+ location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = JSON.stringify(data)
+            }
+        })
     })
 })
