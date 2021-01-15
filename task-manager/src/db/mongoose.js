4b6acf4d82c7b660e743513f4899469bcf6abed5
XiaoshuaiGeng <xiaoshuaigeng@gmail.com>
2021-01-15 01:56:15 -0500

    practice on using mongoose to manipulate mongodb

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,7 +1,83 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        minLength: 6,
+        validate(value) {
+
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password has to be something else')
+            }
+        }
+
+    }
+})
+
+// const me = new User({
+//     name: '   Xiaoshuai     ',
+//     email: 'xiaoshuai@gmail.com      ',
+//     password: 'Ppassword123'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error', error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const test = new Task({
+    description: '    Test    ',
+    
+})
+
+test.save().then(() => {
+    console.log(test)
+}).catch((error) => {
+    console.log(error)
+})
