c9faf35a624bd5a5274c068f74a136ee9910d0b0
XiaoshuaiGeng <xiaoshuaigeng@gmail.com>
2021-01-15 02:49:45 -0500

    practice on REST API

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,50 +1,8 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password: {
-        type: String,
-        trim: true,
-        required: true,
-        minLength: 6,
-        validate(value) {
-
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password has to be something else')
-            }
-        }
-
-    }
 })
 
 // const me = new User({
@@ -71,13 +29,13 @@ const Task = mongoose.model('Task', {
     }
 })
 
-const test = new Task({
-    description: '    Test    ',
+// const test = new Task({
+//     description: '    Test    ',
     
-})
+// })
 
-test.save().then(() => {
-    console.log(test)
-}).catch((error) => {
-    console.log(error)
-})
+// test.save().then(() => {
+//     console.log(test)
+// }).catch((error) => {
+//     console.log(error)
+// })
