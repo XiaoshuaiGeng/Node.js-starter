54495461ac290e0696fc76769701b5e2307aab9f
XiaoshuaiGeng <xiaoshuaigeng@gmail.com>
2021-08-31 14:19:24 +0800

    add DELETE endpoints for /user and /task

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -93,6 +93,20 @@ app.patch('/users/:id', async(req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+
+        if (!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 
 app.post('/task', async (req, res) => {
     const task = new Task(req.body)
@@ -136,7 +150,7 @@ app.get('/task/:id', async (req, res) => {
         }
         res.send(task)
     } catch (error) {
-        res.status(500).send()
+        res.status(500).send(error)
     }
     // Task.findById(req.params.id).then((task) => {
     //     if (!task){
@@ -168,6 +182,20 @@ app.patch('/task/:id', async (req, res) => {
         res.send(task)
     } catch (error) {
         res.status(400).send(error)
+    }
+})
+
+app.delete('/task/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task){
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (error) {
+        res.status(500).send(error)
     }
 })
 
